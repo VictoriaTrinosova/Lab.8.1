36b596432d1e44a8464190dcc423329b69727f60
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-11-29 13:23:55 +0300

    Добавьте файлы проекта.

---
+++ Lab 8.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Виктория/Desktop/Bim проектирование/Lab.8.1/Числа.txt";
+            using (StreamWriter sw = new StreamWriter(path, true)) 
+            { 
+            int[] array = new int[10];
+            Random random = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(0, 20);
+                    sw.WriteLine("{0}", array[i]);
+                   
+                }
+                Console.WriteLine();
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                int s = 0;
+                for (int i=0;i<10;i++)
+                {
+                   int x = Convert.ToInt32(sr.ReadLine());
+                    s += x;
+                    Console.WriteLine(x);
+                }
+                Console.WriteLine("Сумма чисел равна={0}",s);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab.8.1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Lab._8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Виктория/Desktop/Маша";
+            DirectoryInfo directory = new DirectoryInfo(path);
+            DirectoryInfo[] directoryArr = directory.GetDirectories();
+            Console.WriteLine("Список подкаталогов в каталоге");
+            foreach (DirectoryInfo d in directoryArr)
+            {
+                Console.WriteLine(d.Name);
+                FileInfo[] dArr = d.GetFiles();
+                Console.WriteLine("Список файлов в подкаталоге");
+                foreach (FileInfo c in dArr)
+                {
+                    Console.WriteLine(c.Name);
+
+                }
+                Console.WriteLine("Список подкаталогов в подкаталоге");
+                DirectoryInfo[] d1Arr = d.GetDirectories();
+                foreach (DirectoryInfo b in d1Arr)
+                {
+                    Console.WriteLine(b.Name);
+
+                }
+
+            }
+            FileInfo[] dirArr = directory.GetFiles();
+            Console.WriteLine("Список файлов в каталоге");
+            foreach (FileInfo n in dirArr)
+            {
+                Console.WriteLine(n.Name);
+
+            }
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
